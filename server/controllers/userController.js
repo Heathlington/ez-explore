9a3538000bf555e384be44e970a3e4fa3891bf2e
Jack P <jackpeterson99528@gmail.com>
2023-08-14 21:31:52 -0400

    Fixed file structure and modfied User Route

---
+++ server/controllers/userController.js
@@ -0,0 +1,36 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, Saved } = require('../models');
+const { signToken } = require('../utils/auth');
+
+module.exports = {
+    async getUser ({ user = null, params }, res) {
+        const foundUser = await User.findOne({
+         $or: [{ _id: user ? user._id : params.id }, { username: params.username }],   
+        });
+
+        if (!foundUser) {
+            return res.status(400).json({ message: 'Cannot find a user with this id!' })
+        }
+
+        res.json(foundUser);
+    },
+
+    async createUser({ body }, res) {
+        const user = await User.create(body);
+
+        if (!user) {
+            return res.status(400).json({ message: 'Something is wrong!' });
+        }
+        const token = signToken(user);
+        res.json({ token, user });
+    },
+    async login({ body }, res) {
+        const user = await User.findOne({ $or: [{ username: body.username}, { email: body.email }] });
+        if (!user) {
+            return res.status(400).json({ message: "Can't find this user"})
+        }
+        const token = signToken(user);
+        res.json({ token, user });
+    }
+
+}
---
+++ server/models/SavedFile.js
@@ -0,0 +1,51 @@
+const { Schema, Types } = require('mongoose');
+
+const SavedFileSchema = new Schema(
+    {
+        schemaId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        firstName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        destination: {
+            type: String,
+            required: true
+        },
+        length: {
+            type:  Number,
+            required: true
+        },
+        stay: {
+            type: String,
+            required: true
+        },
+        eat: {
+            type: String,
+            required: true
+        },
+        activities: {
+            type: String,
+            required: true
+
+        }
+    },
+    {
+    toJSON: {
+        getters: true,
+    },
+    id: false,
+    }
+);
+
+module.exports = SavedFileSchema;
