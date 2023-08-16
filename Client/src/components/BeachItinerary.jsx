ad250392e3773998db6674a55b20be0f5968effd
jamieholpuch <125837353+jamieholpuch@users.noreply.github.com>
2023-08-16 19:03:14 -0400

    created destination itineraries

--- Client/src/components/BeachItinerary.jsx
+++ Client/src/components/BeachItinerary.jsx
@@ -1,22 +1,51 @@
 export default function BeachItinerary() {
     return (
-    <div>
-      <div className="card" style="width: 18rem;">
-        <img className="card-img-top" src="{image}" alt="Card image cap" />
-        <div className="card-body">
-          <h5 className="card-title">{destination}</h5>
-          <p className="card-text">{destinationDetails}</p>
-      </div>
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">{length}</li>
-        <li className="list-group-item">{stay}</li>
-        <li className="list-group-item">{eat}</li>
-        <li className="list-group-item">{activities}</li>
-      </ul>
-      <div className="card-body">
-        <a href="#" class="card-link">{link}</a>
-      </div>
-    </div>
-    </div>
+<div>
+            <div>
+                <header>
+                    <img src="https://th.bing.com/th/id/R.536ed3e50a4a4bc6a831c966673bf833?rik=4hvnUM9PdVn1Lw&riu=http%3a%2f%2fwww.stayadventurous.com%2fwp-content%2fuploads%2f2017%2f07%2fDestin-Florida-Sunset.jpg&ehk=ln8%2fVFqJG%2b7jM9RRk2%2bXFsHt%2fGEcvZJHzDVjKvClT%2fE%3d&risl=&pid=ImgRaw&r=0" alt="destin sunset" />
+                    <p>Destin, Florida</p>
+                </header>
+            </div>
+            <div className="itinerary-details">
+                <h3>DESTINATION: Destin, Florida</h3>
+                <p className="card-text">Looking for carribean-clear waters without the price tag - or the long flight? Look no further than Destin, Florida. Known as a popular family vacation destination, this panhandle destination is so much more than just a family hot-spot. From entertaining activities, to good eats, and more... Destin has a lot to offer! And who can pass up those fabulous sunsets?</p>
+            </div>
+            <div className="itinerary-grid">
+                <div>
+                    <h4>WHERE TO STAY</h4>
+                    <h5>Budget</h5>
+                    <ul>
+                        <li><img src="" alt="" /></li>
+                        <li>hotel name</li>
+                        <li>average price</li> 
+                    </ul>
+                    <h5>Average</h5>
+                    <ul>
+                        <li><img src="" alt="" /></li>
+                        <li>hotel name</li>
+                        <li>average price</li> 
+                    </ul>
+                    <h5>Bougie</h5>
+                    <ul>
+                        <li><img src="" alt="" /></li>
+                        <li>hotel name</li>
+                        <li>average price</li> 
+                    </ul>
+                </div>
+                <div>
+                    <h4>What to Do</h4>
+                    <img src="tbd" alt="tbd" />
+                </div>
+                <div>
+                    <h4>Where to Eat</h4>
+                    <img src="tbd" alt="tbd" />
+                </div>
+                <div>
+                    <h4>Other Details</h4>
+                    <a href="https://destinflorida.com/" class="card-link">Learn more about Destin</a>
+                </div>
+            </div>
+        </div>
     )
 }
--- Client/src/components/CountrysideItinerary.jsx
+++ Client/src/components/CountrysideItinerary.jsx
@@ -1,22 +1,59 @@
 export default function CountrysideItinerary() {
     return (
-    <div>
-      <div className="card" style="width: 18rem;">
-        <img className="card-img-top" src="{image}" alt="Card image cap" />
-        <div className="card-body">
-          <h5 className="card-title">{destination}</h5>
-          <p className="card-text">{destinationDetails}</p>
-      </div>
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">{length}</li>
-        <li className="list-group-item">{stay}</li>
-        <li className="list-group-item">{eat}</li>
-        <li className="list-group-item">{activities}</li>
-      </ul>
-      <div className="card-body">
-        <a href="#" class="card-link">{link}</a>
-      </div>
-    </div>
-    </div>
+<div>
+            <div>
+                <header>
+                    <img src="" alt="" />
+                    <p>Napa Valley, California</p>
+                </header>
+            </div>
+            <div className="itinerary-details">
+                <h3>DESTINATION: Napa Valley, California</h3>
+                <p className="card-text">insert description here</p>
+            </div>
+            <div className="itinerary-grid">
+                <div>
+                    <h4>WHERE TO STAY</h4>
+                    <h5>Budget</h5>
+                    <ul>
+                        <li><img src="" alt="" /></li>
+                        <li>hotel name</li>
+                        <li>average price</li> 
+                    </ul>
+                    <h5>Average</h5>
+                    <ul>
+                        <li><img src="" alt="" /></li>
+                        <li>hotel name</li>
+                        <li>average price</li> 
+                    </ul>
+                    <h5>Bougie</h5>
+                    <ul>
+                        <li><img src="" alt="" /></li>
+                        <li>hotel name</li>
+                        <li>average price</li> 
+                    </ul>
+                </div>
+                <div>
+                    <h4>WHAT TO DO</h4>
+                    <ul>
+                        <a href=""><li>activity 1</li></a>
+                        <a href=""><li>activity 2</li></a>
+                        <a href=""><li>activity 3</li></a>
+                    </ul>
+                </div>
+                <div>
+                    <h4>WHERE TO EAT</h4>
+                    <ul>
+                        <a href=""><li>restaurant 1</li></a>
+                        <a href=""><li>restaurant 2</li></a>
+                        <a href=""><li>restaurant 3</li></a>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Other Details</h4>
+                    <a href="" class="card-link">Learn more about Napa Valley</a>
+                </div>
+            </div>
+        </div>
     )
 }
