755cb802c92272a2008d4cad355a545abce6df18
Heathlington <126424577+Heathlington@users.noreply.github.com>
2023-08-15 19:19:43 -0500

    created proper files to call on the API

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-
+const userRoutes = require('./userRoutes')
+const itineraryRoutes = require('./itineraryRoutes')
 router.use('/user', userRoutes);
+router.use('/itin', itineraryRoutes);
 
 module.exports = router;
