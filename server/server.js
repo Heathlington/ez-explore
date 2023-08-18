ad0b8cafb4cfd9103688fc205f7be2aa8c680632
jamieholpuch <125837353+jamieholpuch@users.noreply.github.com>
2023-08-17 20:57:39 -0400

    updated server.js and index.js routes

--- server/server.js
+++ server/server.js
@@ -6,9 +6,13 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3020;
 
-app.use(express.static('../client/dist'));
+app.use(express.static('../Client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../Client/dist')));
+}
 
 app.use(routes);
 
